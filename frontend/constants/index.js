dbb89186659810fb8ee3eb6e8723387e780904a2
P-Thakor <pthakor.work@gmail.com>
2024-08-11 18:45:02 +0530

    added header and footer

---
+++ frontend/constants/index.js
@@ -0,0 +1,10 @@
+export const footerLinks = [
+  {
+    links: [
+      { title: "Home", url: "/" },
+      { title: "Events", url: "/" },
+      { title: "Clubs", url: "/" },
+      { title: "About Us", url: "/" },
+    ]
+  }
+];
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        navyblue: '#10107b',
+      },
     },
   },
   plugins: [],

8589c7596e632b4a7e7b2152bbcf1abf740d9156
P-Thakor <pthakor.work@gmail.com>
2024-08-21 14:56:46 +0530

    minor changes

--- frontend/components/AddNewEventTab.jsx
+++ frontend/components/AddNewEventTab.jsx
@@ -5,13 +5,13 @@ const AddNewEventTab = () => {
   return (
     <>
     <div className="flex w-full px-0 py-10">
-      <Image src="/newEvent.png" alt="add new event" width={350} height={350} className="z-10 ml-20"/>
-      <div className="flex absolute bg-navyblue justify-end w-full self-end min-h-40">
-        <div className="text-white px-40 mt-5">
-            <h1 className="text-2xl">Make Your Own Event</h1>
-            <p className="text-xs mt-5">Wanna create an event? Let's do it!</p>
+      <Image src="/newEvent.png" alt="add new event" width={550} height={550} className="z-10 ml-20"/>
+      <div className="flex absolute bg-navyblue justify-center items-center w-full self-end min-h-64">
+        <div className="text-white px-40 ml-32">
+            <h1 className="text-4xl font-semibold">Make Your Own Event</h1>
+            <p className="text-md mt-2 mb-4">Wanna create an event? Let's do it!</p>
             <Link href="/">
-                <button className="custom-btn">
+                <button className="bg-primaryblue px-14 py-4 rounded-md font-semibold">
                     Create Event
                 </button>
             </Link>

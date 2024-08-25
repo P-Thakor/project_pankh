b099e620c13881c68c6e9e7a3646d1acfc49c774
P-Thakor <pthakor.work@gmail.com>
2024-08-25 21:45:31 +0530

    added custom-filter

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,6 @@
-import { AddNewEventTab, EventsList, Hero, EventHero } from "@/components";
+import { AddNewEventTab, EventsList, Hero, EventHero, EventDescription, CustomFilter } from "@/components";
 import events from "@/components/EventPage/dummyData";
+import { Days, EventCategory, EventMode } from "@/constants";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +10,17 @@ export default function Home() {
       <EventHero item={events}/>
       <EventsList />
       <AddNewEventTab />
+      <EventDescription event={events} />
+      <div className="flex w-full flex-col sm:flex-row px-20">
+        <div className="w-1/2">
+          <h1 className="text-3xl font-semibold">Upcoming Events</h1>
+        </div>
+        <div className="w-1/2 flex gap-6">
+          <CustomFilter title="Days" options={Days} />
+          <CustomFilter title="Mode" options={EventMode} />
+          <CustomFilter title="Category" options={EventCategory} />
+        </div>
+      </div>
     </main>
   );
 }

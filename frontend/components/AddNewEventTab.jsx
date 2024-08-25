4e5c9b11c41517936cc4ef420f6e0e85bc4c57eb
P-Thakor <pthakor.work@gmail.com>
2024-08-25 21:46:12 +0530

    minor changes

--- frontend/components/AddNewEventTab.jsx
+++ frontend/components/AddNewEventTab.jsx
@@ -7,11 +7,11 @@ const AddNewEventTab = () => {
     <div className="flex w-full px-0 py-10">
       <Image src="/newEvent.png" alt="add new event" width={550} height={550} className="z-10 ml-20"/>
       <div className="flex absolute bg-navyblue justify-center items-center w-full self-end min-h-64">
-        <div className="text-white px-40 ml-32">
+        <div className="text-white px-40 ml-32 justify-center">
             <h1 className="text-4xl font-semibold">Make Your Own Event</h1>
             <p className="text-md mt-2 mb-4">Wanna create an event? Let's do it!</p>
             <Link href="/">
-                <button className="bg-primaryblue px-14 py-4 rounded-md font-semibold">
+                <button className="bg-primaryblue px-14 py-4 rounded-md font-semibold hover:bg-primarydarkblue">
                     Create Event
                 </button>
             </Link>
--- frontend/components/index.js
+++ frontend/components/index.js
@@ -3,6 +3,8 @@ import Footer from "./Footer";
 import AddNewEventTab from "./AddNewEventTab";
 import EventsList from "./EventsList/EventsList";
 import Hero from "./Hero";
-import EventHero from "./EventPage/EventHero"
+import EventHero from "./EventPage/EventHero";
+import EventDescription from "./EventDescription";
+import CustomFilter from "./CustomFilter";
 
-export { Navbar, Footer, AddNewEventTab, EventsList, Hero, EventHero };
+export { Navbar, Footer, AddNewEventTab, EventsList, Hero, EventHero, EventDescription, CustomFilter };

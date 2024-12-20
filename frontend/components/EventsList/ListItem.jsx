6bd9b661e067c12e1cb4a8ec705693c919a37f2c
P-Thakor <pthakor.work@gmail.com>
2024-12-20 18:22:32 +0530

    improved loader

--- frontend/components/EventsList/ListItem.jsx
+++ frontend/components/EventsList/ListItem.jsx
@@ -4,6 +4,7 @@ import { formattedDate, formattedTime } from "@/utils";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { TailSpin } from "react-loader-spinner";
 
 export default function ListItem({ item }) {
   const router = useRouter();
@@ -13,21 +14,29 @@ export default function ListItem({ item }) {
     setIsLoading(true);
     router.push(url);
   };
+  const date = new Date();
+  const isPast = new Date(item.startDate) < date;
+  console.log(isPast);
 
+  // if (isPast) {
+  //   return null;
+  // } 
   return (
     <>
       {isLoading ? (
-        <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-80 z-50">
-          <span className="text-2xl">Loading...</span>
+        // <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-80 z-50">
+        <div className="items-center justify-center w-96 h-96 pl-44 pt-44">
+          {/* <span className="text-2xl">Loading...</span> */}
+          <TailSpin type="Tailspin" color="#00BFFF" height={50} width={50} />
         </div>
       ) : (
         <div
           className="p-4 w-100 items-center justify-center shadow-xl rounded-xl hover:shadow-2xl hover:scale-105 transition-all duration-300 cursor-pointer"
           onClick={() => handleRedirect(`/view-event/${item._id}`)}
         >
-          <div className="bg-white text-purple-600 rounded-xl px-2 py-1 absolute text-xs m-2">
+          {/* <div className="bg-white text-purple-600 rounded-xl px-2 py-1 absolute text-xs m-2">
             {item.price !== 0 ? "PAID" : "FREE"}
-          </div>
+          </div> */}
           <div className="object-contain">
             <Image
               src={

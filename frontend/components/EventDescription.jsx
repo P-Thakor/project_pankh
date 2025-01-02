a9787d69351bdf84ec374e13eb8db3ed13045f70
P-Thakor <pthakor.work@gmail.com>
2025-01-02 23:02:16 +0530

    update event title and modify description field in EventDescription component

--- frontend/components/EventDescription.jsx
+++ frontend/components/EventDescription.jsx
@@ -4,12 +4,13 @@ const EventDescription = ({ event }) => {
 
 
   const eventData = event;
+  console.log(eventData);
   return (
-    <div className="flex gap-20 flex-wrap p-10 items-center lg:justify-center">
-      <div>
+    <div className="flex gap-20 p-10 items-center">
+      <div className="w-1/2 flex-left">
         <div className="mb-12">
           <h2 className="text-2xl font-bold">Description</h2>
-          <p className="text-gray-500 mt-4">{eventData.description}</p>
+          <p className="text-gray-500 mt-4">{eventData.summary}</p>
         </div>
 
         <div className="mb-4">
@@ -22,7 +23,7 @@ const EventDescription = ({ event }) => {
           </p>
         </div>
       </div>
-      <div>
+      <div className="w-1/2 flex-right">
         <div className="mb-12">
           <h2 className="text-2xl font-bold mb-4">Event Location</h2>
           <p className="text-gray-500">{eventData.locations}</p>

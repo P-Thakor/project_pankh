1e57c87c865607ecf1814a86bafa91974c1befcb
P-Thakor <pthakor.work@gmail.com>
2025-01-06 22:21:41 +0530

    Wrap CreateEventPage with UserContextProvider and enable user fetching in UserContextProvider

--- frontend/app/create-event/page.js
+++ frontend/app/create-event/page.js
@@ -1,8 +1,11 @@
 import { CreateEvent } from "@/components"
+import UserContextProvider from "@/context/UserContextProvider"
 export default function CreateEventPage() {
     return (
+        <UserContextProvider>
         <main>
             <CreateEvent />
         </main>
+        </UserContextProvider>
     )
 }
--- frontend/context/UserContextProvider.jsx
+++ frontend/context/UserContextProvider.jsx
@@ -7,20 +7,20 @@ import { fetchCurrentUser } from "@/utils";
 const UserContextProvider = ({ children }) => {
   const [user, setUser] = React.useState(null);
 
-  // useEffect(() => {
-  //   const fetchUser = async () => {
-  //     try {
-  //       const response = await fetchCurrentUser() 
-  //       // if (response.ok) {
-  //         setUser(response);
-  //       // }
-  //     } catch (error) {
-  //       console.error('Failed to fetch user:', error);
-  //     }
-  //   };
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetchCurrentUser() 
+        // if (response.ok) {
+          setUser(response);
+        // }
+      } catch (error) {
+        console.error('Failed to fetch user:', error);
+      }
+    };
 
-  //   fetchUser();
-  // }, []);
+    fetchUser();
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>

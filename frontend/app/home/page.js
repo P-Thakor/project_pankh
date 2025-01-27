4ad5d5497bdfb4390965241ed820132ae45d3b5c
P-Thakor <pthakor.work@gmail.com>
2025-01-27 14:13:34 +0530

    Implement event categorization in home page with dynamic section display

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { AddNewEventTab, CustomFilter, Hero} from "@/components";
+import { AddNewEventTab, CustomFilter, Hero } from "@/components";
 import List from "@/components/EventsList/List";
 import { Days, EventCategory, EventMode, EventType } from "@/constants";
 import UserContext from "@/context/UserContext";
@@ -9,26 +9,65 @@ import { useContext, useEffect, useState } from "react";
 
 const page = () => {
   const [events, setEvents] = useState([]);
+  const [activeSection, setActiveSection] = useState("Upcoming");
   // const [user, setUser] = useState({});
   const [isAuthorized, setIsAuthorized] = useState(false);
+  const [upcomingList, setUpcomingList] = useState([]);
+  const [pastList, setPastList] = useState([]);
+  const [ongoingList, setOngoingList] = useState([]);
+  let upcomingEvents = [];
+  let ongoingEvents = [];
+  let pastEvents = [];
 
   useEffect(() => {
-    fetchEvents().then((data) => setEvents(data));
+    fetchEvents().then((data) => {
+      setEvents(data);
+      const date = new Date();
+      // const date = today.toISOString();
+      console.log(data);
+      data.forEach((event) => {
+        const startDateObj = new Date(event.startDate);
+        const endDateObj = new Date(event.endDate);
+        if (startDateObj > date) {
+          upcomingEvents.push(event);
+        } else if (startDateObj < date && endDateObj > date) {
+          ongoingEvents.push(event);
+        } else {
+          pastEvents.push(event);
+        }
+      });
+      setUpcomingList(upcomingEvents);
+      setOngoingList(ongoingEvents);
+      setPastList(pastEvents);
+      console.log(upcomingEvents);
+    });
   }, []);
   // useEffect(() => {
-  //   fetchCurrentUser().then((data) => {
-  //     setUser(data);
-  //   });
+  //   const date = new Date();
+  //   console.log(date);
+  //     upcomingEvents = events.filter((event) => event.startDate > date);
+  //     ongoingEvents = events.filter((event) => {
+  //       event.startDate < date && event.endDate > date;
+  //     });
+  //     pastEvents = events.filter((event) => event.endDate < date);
+  //     console.log(events);
+  //     console.log(upcomingEvents);
+  //     console.log(ongoingEvents);
+  //     console.log(pastEvents);
   // }, []);
 
-  const {user} = useContext(UserContext);
+  useEffect(() => {
+    console.log(upcomingList);
+  }, [upcomingList]);
+
+  const { user } = useContext(UserContext);
 
   useEffect(() => {
     if (user?.role === "faculty-member" || user?.role === "admin") {
       setIsAuthorized(true);
     }
     console.log(user);
-  },[]);
+  }, []);
 
   // console.log(isAuthorized);
   // console.log(user.role);
@@ -37,18 +76,39 @@ const page = () => {
       <Hero />
       <div className="flex flex-col items-center justify-between w-full mb-10 sm:px-20 sm:flex-row">
         <div className="flex w-1/2 mb-4 sm:mb-0">
-          <h1 className="mr-3 text-4xl font-semibold">Upcoming</h1>
+          <h1 className="mr-3 text-4xl font-semibold">{activeSection}</h1>
           <h1 className="text-4xl font-semibold text-primaryblue">Events</h1>
         </div>
-        <div className="flex w-1/2 gap-6">
+        {/* <div className="flex w-1/2 gap-6">
           <CustomFilter title="Days" options={Days} />
           <CustomFilter title="Mode" options={EventMode} />
           <CustomFilter title="Category" options={EventCategory} />
           <CustomFilter title="Type" options={EventType} />
+        </div> */}
+        <div className="flex w-1/2 gap-10">
+          <ul className="flex">
+            {["Upcoming", "Ongoing", "Completed"].map((item) => (
+              <li
+                key={item}
+                onClick={() => setActiveSection(item)}
+                className={`p-4 cursor-pointer text-2xl transition-colors text-gray-500 font-semibold ${
+                  activeSection === item
+                    ? "hidden" // Active item style
+                    : "hover:text-primaryblue" // Hover style
+                }`}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
       <div className="pb-10 lg:ml-20">
-      <List list={events} />
+        {activeSection === "Upcoming" && setTimeout(() => {}, 1000) && (
+          <List list={upcomingList} />
+        )}
+        {activeSection === "Ongoing" && <List list={ongoingList} />}
+        {activeSection === "Completed" && <List list={pastList} />}
       </div>
       {isAuthorized && <AddNewEventTab />}
     </main>

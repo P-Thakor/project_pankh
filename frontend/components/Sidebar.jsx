b8e0cade0b321d3a35bbad6a80043ca65ab8f634
hemaxpatel <hemaxpatel3124@gmail.com>
2025-01-28 17:44:35 +0530

    favicon & sidebar logo

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -71,19 +71,18 @@ export default function Sidebar() {
           isOpen ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-300 ease-in-out z-40 rounded-r-lg`}
       >
-        
 
         {/* Nav Links */}
         <ul className="mt-8 space-y-4">
-        {/* <Link href="/home">
+        <Link href="/home">
             <Image
-              src="/logo.svg"
+              src="/logo_cropped.png"
               alt="PANKH logo"
               width={170}
               height={25}
-              className="flex object-contain"
+              className="flex object-contain ml-8"
             />
-          </Link> */}
+          </Link>
         <li
             className="p-4 cursor-pointer hover:text-white hover:bg-primaryblue"
             onClick={() => {

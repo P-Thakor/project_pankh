a7cd7333fc022a60d490c2769ce8362ba344c24c
P-Thakor <pthakor.work@gmail.com>
2025-02-10 23:05:12 +0530

    feat: enhance dashboard and event list components with creator functionality

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,20 +1,25 @@
 import { Profile, EventsCreated, EventsParticipated } from "@/components";
 import EventAttendanceChart from "@/components/EventAttendanceChart";
 import { fetchCurrentUser, fetchEvents } from "@/utils";
+import axios from "axios";
 export default async function Dashboard() {
   const events = await fetchEvents();
-  const user = await fetchCurrentUser();
+  // const res = await axios.get("http://localhost:8001/api/v1/user/me", {
+  //   credentials: "include",
+  // });
+  // const data = await res.json();
+  // console.log(data);
 
   return (
     <main>
       <div className="flex flex-col min-h-screen p-6 space-y-6 bg-gray-100">
         <h1 className="text-3xl font-bold text-primaryblue">Dashboard</h1>
-        { (
+        {
           <div className="flex flex-col space-y-6 md:flex-row md:space-x-6 md:space-y-0">
             <Profile />
             <EventAttendanceChart />
           </div>
-        )}
+        }
         <EventsCreated events={events} />
         <EventsParticipated events={events} />
       </div>
--- frontend/components/EventsList/List.jsx
+++ frontend/components/EventsList/List.jsx
@@ -1,13 +1,13 @@
 import ListItem from "./ListItem";
 
-export default function List({ list = [], style = "", isFaculty = false }) {
+export default function List({ list = [], style = "", isFaculty = false, isCreator = false }) {
   return (
     <ul className={`flex flex-wrap w-full gap-6 justify-center lg:justify-start mb-10 ${style || "lg:ml-14"}`}>
       {list && list.length
         ? list.map((listItem, index) => (
             <li key={index}>
               {" "}
-              <ListItem item={listItem} isFaculty = {isFaculty}/>{" "}
+              <ListItem item={listItem} isFaculty = {isFaculty} isCreator={isCreator}/>{" "}
             </li>
           ))
         : null}

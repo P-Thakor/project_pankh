4efa3a3b74734e41cea3985fce0603094fa79b04
hemaxpatel <hemaxpatel3124@gmail.com>
2025-02-23 12:33:16 +0530

    updated about us and contact us page.

---
+++ frontend/app/about-us/page.js
@@ -0,0 +1,159 @@
+const AboutUsPage = () => {
+  return (
+    <>
+      <div className="p-6 mx-auto bg-white max-w-7xl">
+        <h1 className="mt-5 text-3xl font-bold text-center text-blue-600">
+          About Us
+        </h1>
+        <p className="mt-4 text-lg leading-relaxed text-gray-700">
+          Welcome to <span className="font-semibold text-blue-500">PANKH</span>,
+          the official Event & Club Management Website of{" "}
+          <span className="font-semibold">DEPSTAR</span>. Developed by students
+          of Department of Computer Engineering, designed to revolutionize how
+          events and clubs are managed at our college. This platform replaces
+          traditional offline paperwork with a streamlined digital system,
+          making event organization and participation more efficient than ever.
+        </p>
+        <p className="mt-4 text-lg leading-relaxed text-gray-700">
+          With this platform, students can explore upcoming events, register
+          with ease, and stay updated on various activities. Faculty members can
+          monitor student attendance at events, ensuring better engagement
+          tracking. Additionally, department heads and the principal can access
+          detailed insights into student participation, faculty contributions,
+          and overall event statistics.
+        </p>
+        <p className="mt-4 text-lg leading-relaxed text-gray-700">
+          Our goal is to enhance the student experience, provide seamless event
+          management, and ensure efficient monitoring of college activities.{" "}
+          <span className="font-semibold text-blue-500">
+            Built by students, for students
+          </span>
+          , this platform is a step toward a smarter and more connected campus.
+        </p>
+      </div>
+
+      <h1 className="mt-5 text-3xl font-bold text-center text-blue-600">
+        Project Team
+      </h1>
+      <div className="flex flex-col items-center min-h-screen p-5">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+          <div className="flex flex-col items-center p-4 bg-blue-100 border border-blue-200 rounded-lg shadow-md">
+            <img
+              src="/assets/images/jaysir.jpg"
+              alt="Jay Patel"
+              className="object-cover w-40 h-40 mb-3 rounded-full"
+            />
+            <h1 className="text-2xl">Mr. Jay Patel</h1>
+            <h2 className="mt-1 text-xl">Assistant Professor</h2>
+            <h2 className="text-xl">Mentor</h2>
+            <div className="flex items-center justify-center">
+              <h3 className="text-lg">Phone:</h3>
+              <p className="ml-2 text-blue-600">+91 9428153390</p>
+            </div>
+            <div className="flex justify-center">
+              <h3>Email:</h3>
+              <p className="ml-2 text-blue-600">jaypatel.dce@charusat.ac.in</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center p-4 bg-blue-100 border border-blue-200 rounded-lg shadow-md">
+            <img
+              src="/assets/images/parth.png"
+              alt="Parth Thakor"
+              className="object-cover w-40 h-40 mb-3 rounded-full"
+            />
+            <h1 className="text-2xl">Parth Thakor</h1>
+            <h2 className="mt-1 text-xl">22DCE124</h2>
+            <h2 className="text-xl">Project Manager & Frontend Developer</h2>
+            <div className="flex items-center justify-center">
+              <h3 className="text-lg">Phone:</h3>
+              <p className="ml-2 text-blue-600">+91 7490001678</p>
+            </div>
+            <div className="flex justify-center">
+              <h3>Email:</h3>
+              <p className="ml-2 text-blue-600">22dce124@charusat.edu.in</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center p-4 bg-blue-100 border border-blue-200 rounded-lg shadow-md">
+            <img
+              src="/assets/images/kandarp.jpg"
+              alt="Kandarp Vyas"
+              className="object-cover w-40 h-40 mb-3 rounded-full"
+            />
+            <h1 className="text-2xl">Kandarp Vyas</h1>
+            <h2 className="mt-1 text-xl">22DCE133</h2>
+            <h2 className="text-xl">Backend Developer</h2>
+            <div className="flex items-center justify-center">
+              <h3 className="text-lg">Phone:</h3>
+              <p className="ml-2 text-blue-600">+91 8511123848</p>
+            </div>
+            <div className="flex justify-center">
+              <h3>Email:</h3>
+              <p className="ml-2 text-blue-600">22dce133@charusat.edu.in</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center p-4 bg-blue-100 border border-blue-200 rounded-lg shadow-md">
+            <img
+              src="/assets/images/hemax.png"
+              alt="Hemax Patel"
+              className="object-cover w-40 h-40 mb-3 rounded-full"
+            />
+            <h1 className="text-2xl">Hemax Patel</h1>
+            <h2 className="mt-1 text-xl">22DCE069</h2>
+            <h2 className="text-xl">Frontend Developer</h2>
+            <div className="flex items-center justify-center">
+              <h3 className="text-lg">Phone:</h3>
+              <p className="ml-2 text-blue-600">+91 8488894484</p>
+            </div>
+            <div className="flex justify-center">
+              <h3>Email:</h3>
+              <p className="ml-2 text-blue-600">22dce069@charusat.edu.in</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center p-4 bg-blue-100 border border-blue-200 rounded-lg shadow-md">
+            <img
+              src="/assets/images/namra.png"
+              alt="Namra Vekariya"
+              className="object-cover w-40 h-40 mb-3 rounded-full"
+            />
+            <h1 className="text-2xl">Namra Vekariya</h1>
+            <h2 className="mt-1 text-xl">22DCE131</h2>
+            <h2 className="text-xl">Backend Developer</h2>
+            <div className="flex items-center justify-center">
+              <h3 className="text-lg">Phone:</h3>
+              <p className="ml-2 text-blue-600">+91 7096740206</p>
+            </div>
+            <div className="flex justify-center">
+              <h3>Email:</h3>
+              <p className="ml-2 text-blue-600">22dce131@charusat.edu.in</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center p-4 bg-blue-100 border border-blue-200 rounded-lg shadow-md">
+            <img
+              src="/assets/images/atmiy.png"
+              alt="Atmiy Vithani"
+              className="object-cover w-40 h-40 mb-3 rounded-full"
+            />
+            <h1 className="text-2xl">Atmiy Vithani</h1>
+            <h2 className="mt-1 text-xl">22DCE132</h2>
+            <h2 className="text-xl">UI/UX Designer</h2>
+            <div className="flex items-center justify-center">
+              <h3 className="text-lg">Phone:</h3>
+              <p className="ml-2 text-blue-600">+91 6351818987</p>
+            </div>
+            <div className="flex justify-center">
+              <h3>Email:</h3>
+              <p className="ml-2 text-blue-600">22dce132@charusat.edu.in</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AboutUsPage;
